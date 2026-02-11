fe5094ed411fe9b49d0c2daf59877f8f2713836b
mayronmassingue <mayronmassingue258@mail.com>
2026-02-11 14:18:02 +0200

    feat(src): update App.js
    - 1 file changed: 2 insertions(+), 1 deletions(-)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import { AppBar, Toolbar, Typography, Button, Container } from '@mui/material';
 import './App.css';
 
 // Páginas
